361525a2acdc5d775b38dcc5bc0076e66a7148da
danieljuma5 <jumadaniel275@gmail.com>
2023-03-09 07:10:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+const p1 = 30
+const p2 = 40
+
+function add(p1,p2) {
+  return p1 + p2;
+}
+
+function subtract(p1,p2) {
+  return p1 - p2;
+}
+
+function multiply(p1,p2) {
+  return p1 * p2;
+}
+
+function divide(p1,p2) {
+  return p1/p2;
+}
+
+
+function increment(p1) {
+return p1 + 1;
+}
+function decrement(p1) {
+  return p1 - 1;
+}
+
+function makeInt(p1) {
+  return parseInt(p1,10);
+}
+
+function preserveDecimal(p1) {
+  return parseFloat(p1);
+}
